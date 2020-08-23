d2e5e9f6f2942caec7c364053ed52a9c86467769
Radovan Obradovic <Radovan.Obradovic@gmail.com>
2020-08-23 08:54:54 +0200

    GVAL_THROWS cmake configuration option added.

--- ProgressReporter.cpp
+++ ProgressReporter.cpp
@@ -1,11 +1,16 @@
 #include <ProgressReporter.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdexcept>
 
 void ProgressReporter::error(const std::string &msg) const
 {
+#if GVAL_THROWS
+	throw std::runtime_error(msg);
+#else
 	printf("error:%s\n", msg.c_str());
 	exit(1);
+#endif
 }
 
 void ProgressReporter::warning(const std::string &msg) const
